a16e22511c64ef958564a6d4229a13f883b19e67
GiperBoreipy <evgenchist207@gmail.com>
2025-04-18 17:28:53 +0300

    fix: tests

--- aoot/tests/infra/repositories/sqlalchemy/test_token.py
+++ aoot/tests/infra/repositories/sqlalchemy/test_token.py
@@ -4,7 +4,7 @@ from dishka import AsyncContainer
 
 from infra.adapters.repositories.sqlalchemy import SQLAlchemyTokenRepositoryImpl
 
-from application.ports.transaction_manager import TransactionManager
+from application.ports import TransactionManager
 
 
 @pytest.mark.asyncio
@@ -17,6 +17,8 @@ async def test_sqlalchemy_token_repo(di_container: AsyncContainer) -> None:
 
         token = await token_repo.get_by_ticker("хуй")
         print(token)
+        if token is None:
+            return
         token.mark_as_buyed()
         await tm.commit()
         print(await token_repo.get_by_ticker("хуй"))
